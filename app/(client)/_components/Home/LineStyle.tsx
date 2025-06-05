1fd5e2c579ae8c7bcf677698917e97c860eb2c32
Md Anamul Hasan <anamul15-3625@diu.edu.bd>
2025-06-05 15:11:16 +0600

    Frequently Asked Questions added

--- app/(client)/_components/Home/LineStyle.tsx
+++ app/(client)/_components/Home/LineStyle.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import LineBgIcon from '../Icon/LineBgIcon'
-import Image from 'next/image'
 import lineBg from '@/public/Image/linebg.png'
 
 export default function LineStyle() {
@@ -9,7 +7,7 @@ export default function LineStyle() {
             <div className='w-full bg-cover bg-center bg-no-repeat h-10' style={{ backgroundImage: `url(${lineBg.src})` }}>
 
             </div>
-            <div className='w-full bg-cover bg-center bg-no-repeat h-10' style={{ backgroundImage: `url(${lineBg.src})` }}>
+            <div className='w-full bg-cover bg-center bg-no-repeat h-10 -mt-1' style={{ backgroundImage: `url(${lineBg.src})` }}>
 
             </div>
         </>
