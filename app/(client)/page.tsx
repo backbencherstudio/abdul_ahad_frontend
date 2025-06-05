f9a8f87e6dce2ca1182358bc049e6bf682c64b90
Md Anamul Hasan <anamul15-3625@diu.edu.bd>
2025-06-05 14:32:00 +0600

    Customers say section add

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,11 +1,15 @@
 import HomeBanner from "./_components/Home/Banner";
+import CustomersSay from "./_components/Home/CustomersSay";
 import HowToBook from "./_components/Home/HowToBook";
+import ReadytoBook from "./_components/Home/ReadytoBook ";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center">
       <HomeBanner />
       <HowToBook />
+      <ReadytoBook />
+      <CustomersSay />
     </div>
   );
 }
